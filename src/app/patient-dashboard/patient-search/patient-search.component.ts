000e516cf214bb75e081a440cd2acea7b59523ac
kimaina <kimkenal@gmail.com>
2016-10-24 09:06:37 +0300

    demo demo

--- src/app/patient-dashboard/patient-search/patient-search.component.ts
+++ src/app/patient-dashboard/patient-search/patient-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppFeatureAnalytics } from '../../shared/services/app-feature-analytics.service.ts';
 
 @Component({
   selector: 'app-patient-search',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-search.component.css']
 })
 export class PatientSearchComponent implements OnInit {
+  constructor(private appFeatureAnalytics: AppFeatureAnalytics) {
+  }
 
-  constructor() { }
 
   ngOnInit() {
   }
+
+  public searchPatient(event: any) {
+    this.appFeatureAnalytics.trackEvent('Patient Dashboard', 'Searched Patient', 'searchPatient');
+  }
+
 
 }
